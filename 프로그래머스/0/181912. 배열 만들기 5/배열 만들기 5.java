25275088ff6d2183151d3e9f3cfa649dcde27aec
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-01-22 02:35:52 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.94 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i=0; i<intStrs.length; i++){
+            if(Integer.parseInt(intStrs[i].substring(s, s+l))>k)
+            answer.add(Integer.parseInt(intStrs[i].substring(s, s+l)));
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
