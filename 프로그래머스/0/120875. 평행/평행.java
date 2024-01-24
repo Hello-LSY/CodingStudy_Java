a8170770e8ed72dcea1399acc2b355f60641bab2
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-01-25 05:03:24 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[][] dots) {
+        for (int i = 0; i < dots.length - 1; i++) {
+            for (int j = i + 1; j < dots.length; j++) {
+                double x1 = dots[i][0];
+                double y1 = dots[i][1];
+                double x2 = dots[j][0];
+                double y2 = dots[j][1];
+
+                // 두 점이 동일한 경우
+                if (x1 == x2 && y1 == y2) {
+                    return 1;
+                }
+
+                // 두 점을 이은 직선이 수직이면 스킵
+                if (x1 == x2 || y1 == y2) {
+                    continue;
+                }
+
+                boolean parallel = true;
+
+                for (int k = 0; k < dots.length; k++) {
+                    if (k == i || k == j) {
+                        continue;
+                    }
+
+                    double x3 = dots[k][0];
+                    double y3 = dots[k][1];
+                    double x4 = dots[6 - i - j - k][0];
+                    double y4 = dots[6 - i - j - k][1];
+
+                    if ((y2 - y1) * (x3 - x4) != (y3 - y4) * (x2 - x1)) {
+                        parallel = false;
+                        break;
+                    }
+                }
+
+                if (parallel) {
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
