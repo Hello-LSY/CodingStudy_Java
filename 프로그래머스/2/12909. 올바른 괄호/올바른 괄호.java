772e966b9b5e071fb86fe736f2f5bb86ed399617
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-02-02 19:02:16 +0900

    [level 2] Title: 올바른 괄호, Time: 17.45 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {    
+        Stack<Character>stk = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(c=='('){
+                stk.push(c);
+            }else{
+                if(stk.isEmpty())return false;
+                else stk.pop();
+            }
+        }
+        
+        return stk.isEmpty();
+    }
+}
