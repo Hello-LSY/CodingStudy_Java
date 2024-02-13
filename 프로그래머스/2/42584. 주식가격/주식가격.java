1741faec88dbfc1880b9d21029655e1e95fbcbd8
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-02-13 18:57:46 +0900

    [level 2] Title: 주식가격, Time: 28.26 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,12 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        
-        for(int i=0; i<prices.length; i++){
-            for(int j=i+1; j<prices.length; j++){
-                answer[i]++;
-                if(prices[i]>prices[j])break;
+        Stack<Integer> stock = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stock.isEmpty() && prices[i] < prices[stock.peek()]) {
+                int index = stock.pop();
+                answer[index] = i - index;
             }
+            stock.push(i);
+        }
+
+        while (!stock.isEmpty()) {
+            int index = stock.pop();
+            answer[index] = prices.length - 1 - index;
         }
 
         return answer;
