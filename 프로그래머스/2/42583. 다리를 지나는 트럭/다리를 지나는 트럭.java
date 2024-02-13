ab21b516f7d7770fff10cfa6a8b7fd3be2ed5cb8
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-02-13 17:23:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.14 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0; 
+        Queue<Integer> bridge = new LinkedList<>();
+        Queue<Integer> truck = new LinkedList<>();
+        int nowWeight = 0;
+
+        for (int i : truck_weights) {
+            truck.add(i);
+        }
+
+        while (!truck.isEmpty()) {
+            answer++;
+
+            if (bridge.size() == bridge_length) { 
+                nowWeight -= bridge.poll();
+            }
+            
+            if (nowWeight + truck.peek() <= weight) {
+                int truckWeight = truck.poll();
+                nowWeight += truckWeight;
+                bridge.add(truckWeight);
+            } else {
+                bridge.add(0);
+            }
+        }
+
+        answer += bridge_length;
+        return answer;
+    }
+}
