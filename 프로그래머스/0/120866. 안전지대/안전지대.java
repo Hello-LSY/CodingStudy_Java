8fa2589e979bd3d1aceeaadb8f6b6fcdb4420288
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-02-14 16:32:09 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -2,27 +2,29 @@ class Solution {
     public int solution(int[][] board) {
         int answer = 0;
         
-        int[][]bomb = new int[board.length+2][board.length+2];
-        int[][] check = {{-1, -1}, {0, -1}, {1, -1}, 
-                         {-1, 0}, {0, 0}, {1, 0}, 
-                         {-1, 1}, {0, 1}, {1, 1}};
+        int[][] flag = {{-1, -1}, {0, -1}, {1, -1},
+                        {-1, 0}, {0, 0}, {1, 0},
+                        {-1, 1}, {0, 1}, {1, 1}};
         
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                if(board[i][j]==1){
-                    for(int k=0; k<9; k++)
-                        bomb[i+check[k][0]+1][j+check[k][1]+1]++;
+        int[][] check = new int[board.length + 2][board.length + 2];
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[i][j] == 1) {
+                    for (int k = 0; k < flag.length; k++) {
+                        int x = flag[k][1];
+                        int y = flag[k][0];
+                        check[i + y + 1][j + x + 1]++;
+                    }
                 }
             }
         }
         
-
-        for(int i=1; i<bomb.length-1; i++){
-            for(int j=1; j<bomb[0].length-1; j++){
-                if(bomb[i][j]==0)answer++;
+        for (int i = 1; i < check.length-1; i++) {
+            for (int j = 1; j < check.length-1; j++) {
+                if (check[i][j]==0) answer++;
             }
         }
-        
         
         return answer;
     }
