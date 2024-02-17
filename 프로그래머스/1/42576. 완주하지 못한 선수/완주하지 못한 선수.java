1b69d15b9e1a0da5e39c2e100cfc4abfafe3b809
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-02-17 22:58:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.51 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,29 +1,22 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
 
-        HashMap<String, Integer> participantMap = new HashMap<>();
-
-        //map에 참가자 담기(중복이면 +1)
-        for (String p : participant) {
-            participantMap.put(p, participantMap.getOrDefault(p, 0) + 1);
+        Map<String, Integer> passMap = new HashMap<>();
+        for(String part : participant){
+            passMap.put(part, passMap.getOrDefault(part, 0)+1);
+        }
+        
+        for(String comp : completion){
+            passMap.replace(comp, passMap.get(comp)-1);
         }
 
-        //map의 value에 완주자만큼 빼기
-        for (String c : completion) {
-            participantMap.put(c, participantMap.get(c) - 1);
+        for(Map.Entry<String, Integer>entry : passMap.entrySet()){
+            if(entry.getValue()!=0)return answer = entry.getKey();
         }
-
-        //key가 하나이상 = 완주못한사람 or 중복된 사람
-        for (String key : participantMap.keySet()) {
-            if (participantMap.get(key) > 0) {
-                answer = key;
-                break;
-            }
-        }
-
+        
         return answer;
     }
 }
