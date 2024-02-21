e204f33e7c10ec90e28305cbbec80b37389c5935
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-02-22 00:44:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.05 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,19 +4,24 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
 
+        // <참가자의 이름,참가 횟수>
         Map<String, Integer> passMap = new HashMap<>();
-        for(String part : participant){
-            passMap.put(part, passMap.getOrDefault(part, 0)+1);
-        }
-        
-        for(String comp : completion){
-            passMap.replace(comp, passMap.get(comp)-1);
+        for (String part : participant) {
+            passMap.put(part, passMap.getOrDefault(part, 0) + 1);
         }
 
-        for(Map.Entry<String, Integer>entry : passMap.entrySet()){
-            if(entry.getValue()!=0)return answer = entry.getKey();
+        // 완주한 선수들의 참가 횟수 감소
+        for (String comp : completion) {
+            passMap.replace(comp, passMap.get(comp) - 1);
         }
-        
+
+        // 남아있는 횟수가 0이 아닌 선수를 찾아서 반환
+        for (Map.Entry<String, Integer> entry : passMap.entrySet()) {
+            if (entry.getValue() != 0) {
+                return answer = entry.getKey();
+            }
+        }
+
         return answer;
     }
 }
