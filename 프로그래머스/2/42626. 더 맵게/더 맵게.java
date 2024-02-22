629abe67e5ca3d12a3e0d95fb07b03bd79faf59d
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-02-22 21:23:36 +0900

    [level 2] Title: 더 맵게, Time: 1460.83 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,21 +2,27 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer=0;
+        int answer = 0;
+        // 힙 구현
         PriorityQueue<Integer> scvPQ = new PriorityQueue<>();
         
-        for(int i : scoville){
+        for (int i : scoville) {
             scvPQ.add(i);
         }
         
-        while(scvPQ.peek()<K && scvPQ.size()!=1){
+        // 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복
+        while (scvPQ.peek() < K && scvPQ.size() != 1) {
+    
             int fQ = scvPQ.poll();
             int sQ = scvPQ.poll();
-            int scvilleScore = fQ + (sQ*2);
+            int scvilleScore = fQ + (sQ * 2);
+            // 섞은 음식의 스코빌 지수를 우선순위 큐에 추가
             scvPQ.offer(scvilleScore);   
+            // 섞은 횟수 증가
             answer++;
         }
         
-        return scvPQ.peek()>=K ? answer : -1;
+        
+        return scvPQ.peek() >= K ? answer : -1;
     }
 }
