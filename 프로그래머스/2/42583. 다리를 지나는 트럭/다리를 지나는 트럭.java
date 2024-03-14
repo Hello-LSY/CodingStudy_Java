25a5dee45bb0333ad4cdff4a679eaed6eba8f7b2
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-03-14 21:23:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 2.42 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,33 +1,41 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0; 
-        Queue<Integer> bridge = new LinkedList<>();
-        Queue<Integer> truck = new LinkedList<>();
-        int nowWeight = 0;
+        //트럭을 저장하는 스택 
+        Stack<Integer> truckStack = new Stack<>();
+        //다리 위의 트럭 상태
+        Map<Integer, Integer> bridgeMap = new HashMap<>();
 
-        for (int i : truck_weights) {
-            truck.add(i);
+        // 트럭을 스택에 넣기
+        for (int i = truck_weights.length - 1; i >= 0; i--)
+            truckStack.push(truck_weights[i]);
+        
+        // 경과시간
+        int time = 0;
+        //현재 다리 위에 있는 트럭의 무게
+        int sum = 0;
+        // 모든 트럭이 다리를 건널 때까지 
+        while (true) {
+            time++; //경과 시간 증가
+
+            // 다리를 건너고 있는 트럭 중, 도착한 트럭은 제거
+            if (bridgeMap.containsKey(time))
+                bridgeMap.remove(time);
+
+            sum = bridgeMap.values().stream().mapToInt(Number::intValue).sum();
+
+            //다리에 트럭을 추가해도 무게 제한을 초과하지 않으면 트럭 추가
+            if (!truckStack.isEmpty())
+                if (sum + truckStack.peek() <= weight)
+                    bridgeMap.put(time + bridge_length, truckStack.pop());
+
+            // 다리와 대기 중인 트럭이 모두 비어있으면 종료
+            if (bridgeMap.isEmpty() && truckStack.isEmpty())
+                break;
         }
-
-        while (!truck.isEmpty()) {
-            answer++;
-
-            if (bridge.size() == bridge_length) { 
-                nowWeight -= bridge.poll();
-            }
-
-            if (nowWeight + truck.peek() <= weight) {
-                int truckWeight = truck.poll();
-                nowWeight += truckWeight;
-                bridge.add(truckWeight);
-            } else {
-                bridge.add(0);
-            }
-        }
-
-        answer += bridge_length;
-        return answer;
+        return time; 
     }
 }
