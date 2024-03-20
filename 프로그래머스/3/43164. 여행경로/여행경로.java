87d4ccf113cab43588b4000108217f87010fbba6
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-03-20 17:55:26 +0900

    [level 3] Title: 여행경로, Time: 12.88 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 class Solution {
     
-    PriorityQueue<String>pQ; 
+    PriorityQueue<String> pQ;
     boolean[] visited;
     
     public String[] solution(String[][] tickets) {
+        
         String[] answer = {};
         pQ = new PriorityQueue<>();
         visited = new boolean[tickets.length];
@@ -16,20 +17,20 @@ class Solution {
     }
     
     public void dfs(String start, String route, String[][] tickets, int cnt){
-        
-        if(cnt==tickets.length){
+
+        if(tickets.length == cnt){
             pQ.add(route);
             return;
         }
         
         for(int i=0; i<tickets.length; i++){
-            if(tickets[i][0].equals(start) && !visited[i]){
-                visited[i] = true;
+            if(!visited[i] && tickets[i][0].equals(start) ){
+                visited[i]=true;
                 dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt+1);
                 visited[i]=false;
             }
+            
         }
-        
         
     }
 }
