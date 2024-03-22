f8eefb8292ed5c0a4d7da374af0256c72e27347f
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-03-22 22:12:25 +0900

    [level 3] Title: 등굣길, Time: 0.70 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -7,12 +7,11 @@ class Solution {
         for(int[] puddle : puddles){
             int x = puddle[0]-1;
             int y = puddle[1]-1;
-            route[y][x]=-1;
+            route[y][x] = -1;
         }
-        
         route[0][0]=1;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<route.length; i++){
+            for(int j=0; j<route[i].length; j++){
                 if(route[i][j]==-1){
                     route[i][j]=0;
                     continue;
@@ -23,9 +22,12 @@ class Solution {
                 if(j>0){
                     route[i][j]+=route[i][j-1] % 1000000007;
                 }
-            }
+            }   
         }
+        
         answer = route[n-1][m-1] % 1000000007;
+
+        
         return answer;
     }
 }
