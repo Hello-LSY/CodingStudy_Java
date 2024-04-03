ed2234eb3f40771d97c9d1b916acaf571ad2a0d6
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-04-03 09:27:16 +0900

    [level 1] Title: 모의고사, Time: 3.86 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,35 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-
         int[] giveUpMath1 = {1,2,3,4,5};
         int[] giveUpMath2 = {2,1,2,3,2,4,2,5};
         int[] giveUpMath3 = {3,3,1,1,2,2,4,4,5,5};
 
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
-
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == giveUpMath1[i % giveUpMath1.length]) cnt1++;
-            if (answers[i] == giveUpMath2[i % giveUpMath2.length]) cnt2++;
-            if (answers[i] == giveUpMath3[i % giveUpMath3.length]) cnt3++;
+        int cnt1=0;
+        int cnt2=0;
+        int cnt3=0;
+        
+        for(int i=0; i<answers.length; i++){
+            if(giveUpMath1[i%giveUpMath1.length]==answers[i]) cnt1++;
+            if(giveUpMath2[i%giveUpMath2.length]==answers[i]) cnt2++;
+            if(giveUpMath3[i%giveUpMath3.length]==answers[i]) cnt3++;
         }
-
-        int maxCnt = Math.max(cnt1, Math.max(cnt2, cnt3));
-
-        List<Integer> resultList = new ArrayList<>();
-
-        if (maxCnt == cnt1) resultList.add(1);
-        if (maxCnt == cnt2) resultList.add(2);
-        if (maxCnt == cnt3) resultList.add(3);
-
-        answer = new int[resultList.size()];
-
-        for (int i = 0; i < resultList.size(); i++) {
-            answer[i] = resultList.get(i);
+        
+        List<Integer> list = new ArrayList<>();
+        list.add(cnt1);
+        list.add(cnt2);
+        list.add(cnt3);
+        
+        int highScore = Math.max(Math.max(cnt1, cnt2), cnt3);
+        List<Integer> result = new ArrayList<>();
+        
+        for(int i=0; i<3; i++){
+            if(list.get(i) == highScore) {
+                result.add(i+1); 
+            }
         }
-
-        return answer;
+        
+        return result.stream().mapToInt(i->i).toArray();
     }
 }
