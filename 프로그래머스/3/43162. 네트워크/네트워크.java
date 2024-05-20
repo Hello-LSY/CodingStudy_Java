802e0a3aed069437b2028246de68d2e0830753de
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-05-20 13:58:04 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,37 +1,36 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        
         boolean[] visited = new boolean[n];
-        
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                bfs(i,n,computers,visited);
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(i, n, computers, visited);
                 answer++;
             }
         }
-        
         return answer;
     }
-    
-    private void bfs(int start, int n, int[][] computers, boolean[] visited){
+
+    public static void bfs(int start, int n, int[][] computers, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(start);
         visited[start] = true;
-        
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
-            visited[cur] = true;
-            
-            for(int i=0; i<n; i++){
-                if(computers[i][cur]==1 && !visited[i]){
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int i = 0; i < n; i++) {
+                if (computers[current][i] == 1 && !visited[i]) {
                     queue.offer(i);
-                    visited[i]=true;
+                    visited[i] = true;
                 }
             }
         }
-        
     }
-    
+
+
 }
