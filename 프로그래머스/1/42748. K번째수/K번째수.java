762a413e607a8a31fa4b62a44bbf2777964888e2
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-06-20 10:20:06 +0900

    [level 1] Title: K번째수, Time: 0.92 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,16 +1,16 @@
 import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-    
+        
         for(int i=0; i<commands.length; i++){
             int start = commands[i][0]-1;
             int end = commands[i][1];
-            int idx = commands[i][2]-1;
-            int[] cut_arr = Arrays.copyOfRange(array, start, end);
-            Arrays.sort(cut_arr);
-            answer[i] = cut_arr[idx];
+            int find = commands[i][2]-1;
+            
+            int[] cArr = Arrays.copyOfRange(array, start, end);
+            Arrays.sort(cArr);
+            answer[i] = cArr[find];
         }
         
         return answer;
