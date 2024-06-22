93a885cf8df8d1ce2785b5a04612ec1bb0b990f3
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-06-22 14:37:45 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,35 +1,35 @@
 import java.util.*;
 
 class Solution {
-    public static boolean[] visited;
     
+    public static boolean[] visited;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n];   
-        
+        visited = new boolean[n];
         for(int i=0; i<n; i++){
             if(!visited[i]){
-                bfs(i, n, visited, computers);
+                bfs(i, n, computers);
                 answer++;
             }
-        }            
+        }
+        
         return answer;
-    }    
+    }
     
-    public static void bfs(int start, int n, boolean[] visited, int[][] computers){
+    public void bfs(int start, int n, int[][] computers){
         Queue<Integer> que = new LinkedList<>();
-        que.offer(start);
-        visited[start] = true; 
+        que.add(start);
+        visited[start]=true;
         
         while(!que.isEmpty()){
-            int cur = que.poll();   
+            int cur = que.poll();
             
             for(int i=0; i<n; i++){
-                if(computers[cur][i] == 1 && !visited[i]){
-                    que.offer(i);
+                if(!visited[i] && computers[cur][i]==1){
+                    que.add(i);
                     visited[i]=true;
                 }
             }
-        }       
+        }
     }
 }
