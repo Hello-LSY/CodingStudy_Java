796bc83aa4bdd28fd1c3f0ef79d55154bc24238e
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-06-22 13:19:32 +0900

    [level 2] Title: 타겟 넘버, Time: 0.39 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,50 +1,21 @@
-import java.util.*;
 class Solution {
-    int answer = 0;
-    int[] cal = {1, -1};
+    static int answer;
+    
     public int solution(int[] numbers, int target) {
-
-        // dfs(numbers, target, 0, 0);
+        answer = 0;
         
-        answer= bfs(numbers, target);
+        dfs(numbers, target, 0, 0);
         
         return answer;
     }
     
-    private int bfs(int []numbers, int target){
-        int depth = 1;
+    private void dfs(int[] numbers, int target, int depth, int sum){
         
-        Queue<Integer> queue = new LinkedList<>();
-        for(int calculate : cal){
-            queue.offer(numbers[0]*calculate);
+        if(depth==numbers.length){
+            if(target==sum)answer++;
+        }else{
+            dfs(numbers, target, depth+1, sum+numbers[depth]);
+            dfs(numbers, target, depth+1, sum-numbers[depth]);
         }
-        
-        while(depth < numbers.length){
-            int size = queue.size();
-            for(int i=0; i<size; i++){
-                int cur = queue.poll();
-                for(int calculate : cal) 
-                    queue.offer(cur + numbers[depth]*calculate);
-            }
-            depth++;
-        }
-        
-        while(!queue.isEmpty()){
-            if(queue.poll()==target) answer++;
-        }
-        
-        return answer++;
-        
     }
-        
-//     private void dfs(int[]numbers, int target, int depth, int sum){
-        
-//         if(depth==numbers.length){
-//             if(target==sum) answer++;
-//         }
-//         else{
-//             dfs(numbers, target, depth+1, sum+numbers[depth]);
-//             dfs(numbers, target, depth+1, sum-numbers[depth]);
-//         }
-//     }
 }
