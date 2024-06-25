935dba1129dca420dc173fff1eb1082d6317c9f5
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-06-25 17:32:43 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.62 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,26 +1,58 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = {0,0};
-        
+        // 두 개의 우선순위 큐를 사용하여 최솟값과 최댓값을 추적
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        HashMap<Integer, Integer> countMap = new HashMap<>();
         
         for(String operation : operations){
             String[] command = operation.split(" ");
             String alpha = command[0];
             int digit = Integer.parseInt(command[1]);
-            if(alpha.equals("I")){
+
+            if (alpha.equals("I")) {
+                // 숫자 삽입
                 minHeap.add(digit);
                 maxHeap.add(digit);
-            }else if(alpha.equals("D")){
-                if(digit==1){
-                    minHeap.remove(maxHeap.poll());
-                }else
-                    maxHeap.remove(minHeap.poll());
+                countMap.put(digit, countMap.getOrDefault(digit, 0) + 1);
+            } else if (alpha.equals("D")) {
+                if (digit == 1) {
+                    // 최댓값 삭제
+                    removeElement(maxHeap, countMap);
+                } else {
+                    // 최솟값 삭제
+                    removeElement(minHeap, countMap);
+                }
             }
         }
-       
-        return maxHeap.isEmpty() ? answer : new int[]{maxHeap.poll(), minHeap.poll()};
+
+        // 결과 계산
+        if (countMap.isEmpty()) {
+            return new int[]{0, 0};
+        }else{
+            int max = removeElement(maxHeap, countMap);
+            int min = removeElement(minHeap, countMap);
+
+            return new int[]{max, min};
+        }
+
+    }
+
+    private int removeElement(PriorityQueue<Integer> heap, HashMap<Integer, Integer> countMap) {
+        while (!heap.isEmpty()) {
+            int element = heap.poll();
+            int count = countMap.getOrDefault(element, 0);
+            if (count > 0) {
+                if (count == 1) {
+                    countMap.remove(element);
+                } else {
+                    countMap.put(element, count - 1);
+                }
+                return element;
+            }
+        }
+        return 0;
     }
 }
