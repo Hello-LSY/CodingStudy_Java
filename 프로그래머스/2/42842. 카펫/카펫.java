d4a8b904891c728b2b40b9c2fc58fd0e068080f3
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-06-26 10:13:18 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,19 +2,19 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int carpet = brown + yellow;
+        //일단 col, row를 구하면 (col-2) * (row-2) 으로 yellow구역 구할 수 있다 
+        //조건으로 주어진건 카펫의 row길이>=col길이
         
-        for(int w=3; w<carpet; w++){
-            int h= carpet/w;
-            
-            if(carpet % w ==0 && h>=3){
-                int col = Math.max(w,h);
-                int row = Math.min(w,h);
-                int center = (col-2) * (row-2);
-                
-                if(center ==yellow){
-                    answer[0] = col;
-                    answer[1] = row;
-                }
+        int w = 3;
+        while(w<carpet){
+            int col = w;
+            int row = carpet/w;
+            if((col-2)*(row-2)==yellow && row>=col){
+                answer[0] = row;
+                answer[1] = col;
+                break;
+            }else{
+                w++;
             }
         }
         
