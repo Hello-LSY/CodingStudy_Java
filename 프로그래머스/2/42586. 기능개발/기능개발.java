52c94fa4b81ad71b3cd1a7251445359f2dc6025a
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-07-02 10:11:05 +0900

    [level 2] Title: 기능개발, Time: 0.44 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,49 +1,30 @@
 import java.util.*;
 
 class Solution {
-    
-    public class Progress {
-        int progress;
-        int speed;
-        
-        public Progress(int progress, int speed){
-            this.progress = progress;
-            this.speed = speed;
-        }
-
-        public int getRemainingDays() {
-            return (int)Math.ceil((100.0 - progress) / speed);
-        }
-    }
-    
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Progress> queue = new LinkedList<>();
+        List<Integer> answerList = new ArrayList<>();
+        Queue<Integer> que = new LinkedList<>();
+        
         for (int i = 0; i < progresses.length; i++) {
-            queue.add(new Progress(progresses[i], speeds[i]));
+            int days = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            que.add(days);
         }
-
-        List<Integer> resultList = new ArrayList<>();
-
-        while (!queue.isEmpty()) {
-            // 첫 번째 기능의 남은 일수 계산
-            int days = queue.peek().getRemainingDays();
-
-            // 해당 일수 동안 모든 기능 업데이트
-            for (Progress progress : queue) {
-                progress.progress += progress.speed * days;
-            }
-
-            int count = 0;
-            while (!queue.isEmpty() && queue.peek().progress >= 100) {
-                queue.poll();
+        
+        while (!que.isEmpty()) {
+            int current = que.poll();
+            int count = 1;
+            
+            while (!que.isEmpty() && que.peek() <= current) {
+                que.poll();
                 count++;
             }
-            resultList.add(count);
+            
+            answerList.add(count);
         }
         
-        int[] answer = new int[resultList.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = resultList.get(i);
+        int[] answer = new int[answerList.size()];
+        for (int i = 0; i < answerList.size(); i++) {
+            answer[i] = answerList.get(i);
         }
         
         return answer;
