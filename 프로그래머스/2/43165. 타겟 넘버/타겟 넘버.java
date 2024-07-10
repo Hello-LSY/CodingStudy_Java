904f2fb713e3fa30d468872c880c04f8e033456d
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-07-10 10:14:32 +0900

    [level 2] Title: 타겟 넘버, Time: 0.35 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,21 +1,19 @@
 class Solution {
     static int answer;
-    
     public int solution(int[] numbers, int target) {
         answer = 0;
         
         dfs(numbers, target, 0, 0);
-        
+    
         return answer;
     }
     
-    private void dfs(int[] numbers, int target, int depth, int sum){
-        
+    private void dfs(int[] numbers, int target, int depth, int result){
         if(depth==numbers.length){
-            if(target==sum)answer++;
+            if(result==target) answer++;
         }else{
-            dfs(numbers, target, depth+1, sum+numbers[depth]);
-            dfs(numbers, target, depth+1, sum-numbers[depth]);
-        }
+                dfs(numbers, target, depth+1, result+numbers[depth]);
+                dfs(numbers, target, depth+1, result-numbers[depth]);
+        }         
     }
 }
