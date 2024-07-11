d6e8a5b2e5f71e62c7de5d7746fa166f488b3452
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-07-11 10:58:57 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,41 +1,38 @@
 class Solution {
+    private static boolean visited[];
+    private static int minStep;
+        
     public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+        minStep = Integer.MAX_VALUE;
+        dfs(begin, target, words, 0);
         
-        int answer = dfs(begin, target, words, new boolean[words.length], 0, Integer.MAX_VALUE);
-        return answer == Integer.MAX_VALUE ? 0 : answer;
+        return minStep == Integer.MAX_VALUE ? 0 : minStep;
     }
-
     
-    private int dfs(String current, String target, String[] words, boolean[] visited, int steps, int minSteps) {
-        
-        if (current.equals(target)) {
-            return Math.min(minSteps, steps);
+    private void dfs(String cur, String target, String[] words, int step) {
+        if (cur.equals(target)) {
+            minStep = Math.min(step, minStep);
+            return;
         }
-
-        // 다음 단어 탐색
+        
         for (int i = 0; i < words.length; i++) {
-            // 방문하지 않았고, 현재 단어와 다른 단어 중 한 문자만 다른 경우
-            if (!visited[i] && isOneCharDifferent(current, words[i])) {
-                visited[i] = true; // 방문 표시
-                minSteps = dfs(words[i], target, words, visited, steps + 1, minSteps); // DFS 호출
-                visited[i] = false; // 백트래킹
+            if (!visited[i] && cmpStr(cur, words[i])) {
+                visited[i] = true;
+                dfs(words[i], target, words, step + 1);
+                visited[i] = false;  
             }
         }
-
-        return minSteps;
     }
-
-    // 두 단어가 한 문자만 다른지 여부 확인
-    private boolean isOneCharDifferent(String s1, String s2) {
-        int diffCount = 0;
+    
+    // 한 글자만 다른지 확인
+    private boolean cmpStr(String s1, String s2) {
+        int cnt = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                diffCount++;
-                if (diffCount > 1) {
-                    return false;
-                }
+                cnt++;
             }
         }
-        return diffCount == 1;
+        return cnt == 1;
     }
 }
