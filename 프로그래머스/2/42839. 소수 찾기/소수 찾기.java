aed41e4b073f477d4a0318a422064dcc0bfc525a
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-07-12 11:35:10 +0900

    [level 2] Title: 소수 찾기, Time: 11.07 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -36,15 +36,12 @@ class Solution {
     }
 
     // 소수 여부 확인
-    private boolean isPrime(int n) {
-        if (n < 2) {
-            return false;
+    private boolean isPrime(int num){
+        if(num<2)return false;
+        for(int i = 2; i<Math.sqrt(num); i++){
+            if(num%i==0)return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
+        
         return true;
     }
 }
