7adc80c9c5b3307dd5806d68ff3e18bbf10eed5d
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-08 17:46:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.91 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,52 +1,41 @@
 import java.util.*;
 
 class Solution {
-    
-    int[] moveX = {-1, 1, 0, 0};
-    int[] moveY = {0, 0, 1, -1};
+    public static final int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    public static boolean[][] visited;
     
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        boolean[][] visited = new boolean[n][m];
-        
-        int answer = bfs(maps, visited, n, m);
-        
-        return answer;
+        visited = new boolean[maps.length][maps[0].length];
+        return bfs(maps);
     }
     
-    public int bfs(int[][] maps, boolean[][] visited, int n, int m) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1}); // 시작 지점 (0, 0)과 거리 1
-        
+    private int bfs(int[][] maps){
+        Queue<int[]>que = new LinkedList<>();
+        // 처음 좌표기준 +1
+        que.offer(new int[]{0,0,1});
         visited[0][0] = true;
-        
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-            int distance = cur[2];
+        while(!que.isEmpty()){
+            int[] cur = que.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int cnt = cur[2];
             
-            // 목적지에 도착했을 때 거리 반환
-            if (curX == n - 1 && curY == m - 1) {
-                return distance;
-            }
-            
-            // 네 방향으로 이동
-            for (int i = 0; i < 4; i++) {
-                int nextX = curX + moveX[i];
-                int nextY = curY + moveY[i];
-                
-                // 다음 위치가 범위 안에 있고, 벽이 아니며, 방문하지 않았다면 이동
-                if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m && maps[nextX][nextY] == 1 && !visited[nextX][nextY]) {
-                    queue.add(new int[]{nextX, nextY, distance + 1});
-                    visited[nextX][nextY] = true;
-                }
+            if(x==maps.length-1 && y==maps[0].length-1){
+                 return cnt;
+            }else{
+               for(int i=0; i<4; i++){
+                   int dx = x+dir[i][0];
+                   int dy = y+dir[i][1];
+                   if(dx<maps.length && dy<maps[0].length && dx>=0 && dy>=0 &&
+                      !visited[dx][dy] && maps[dx][dy]!=0){
+                       que.offer(new int[]{dx, dy, cnt+1});
+                       visited[dx][dy] = true;
+                   }
+               } 
             }
         }
-        
-        // 도착할 수 없는 경우
         return -1;
+
     }
+    
 }
