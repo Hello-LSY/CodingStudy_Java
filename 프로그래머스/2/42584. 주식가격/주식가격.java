dd362c8e4938f2fb7bae41725e1845e249410e54
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-08 17:24:15 +0900

    [level 2] Title: 주식가격, Time: 80.54 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,26 +1,25 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
 
-        // 주식 가격 순회
-        for (int i = 0; i < prices.length; i++) {
-            // 주식 가격이 현재 가격보다 클 경우
-            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int idx = stack.pop();
-                // 인덱스 갱신
-                answer[idx] = i - idx;
+        // 시간 저장
+        Stack<Integer> stk = new Stack<>();
+        // n번 완탐
+        for(int i=0; i<prices.length; i++){
+            
+            // 스택 최상단에 있는 값(i 시점의 가격) > 현재 주식가격 이면 시간계산
+            while(!stk.isEmpty() && prices[i]<prices[stk.peek()]){
+                answer[stk.peek()] = i - stk.pop();
             }
-            stack.push(i);
+            stk.push(i);
         }
-
-        while (!stack.isEmpty()) {
-            int idx = stack.pop();
-            answer[idx] = prices.length - idx - 1;
+        
+        // 남은 주식들 = 변동없으므로 prices크기 -1 에서 시점을 빼서 갱신
+        while(!stk.isEmpty()){
+            answer[stk.peek()] = prices.length - stk.pop() - 1;
         }
-
+        
         return answer;
     }
 }
