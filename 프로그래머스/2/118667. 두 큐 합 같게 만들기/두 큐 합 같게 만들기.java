bc2241b3771976715ead5aebf3571066e9e5a4d4
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-09 10:45:13 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 94.40 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,45 +1,40 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+        int n = queue1.length;
+        long total1 = 0;
+        long total2 = 0;
         Queue<Integer> que1 = new LinkedList<>();
         Queue<Integer> que2 = new LinkedList<>();
         
-        long sum1 = 0, sum2 = 0; 
-        for (int i = 0; i < queue1.length; i++) {
-            que1.add(queue1[i]);
-            sum1 += queue1[i];
-            que2.add(queue2[i]);
-            sum2 += queue2[i];
+        for(int i=0; i<n; i++){
+            que1.offer(queue1[i]);
+            que2.offer(queue2[i]);
+            total1+=queue1[i];
+            total2+=queue2[i];
         }
-
-        long totalSum = sum1 + sum2;
-        // 총합이 홀수면 반으로 나눌 수 없으므로 -1 반환
-        if (totalSum % 2 != 0) return -1;
         
-        long target = totalSum / 2;
-        int answer = 0;
-        int limit = queue1.length * 2 + 1;
+        long total = total1 + total2;
+        long target = total/2;
+        int limit = n*2 + 1;
         
-        // 투 포인터 방식으로 큐를 다룸
-        while (sum1 != target && answer <= limit) {
-            // 1 -> 2 
-            if (sum1 > target) {
+        while(total1!=target && answer<=limit){
+            
+            if(total1>target){
                 int value = que1.poll();
-                sum1 -= value;
-                que2.add(value);
-                sum2 += value;
-            // 2 -> 1
-            } else {
+                total1-=value;
+                total2+=value;
+                que2.offer(value);
+            }else{
                 int value = que2.poll();
-                sum2 -= value;
-                que1.add(value);
-                sum1 += value;
+                total2-=value;
+                total1+=value;
+                que1.offer(value);
             }
             answer++;
         }
         
-        // 다 해도 합이 맞지 않으면 -1 반환
-        return sum1 == target ? answer : -1;
+        return total1== target ? answer : -1;
     }
 }
