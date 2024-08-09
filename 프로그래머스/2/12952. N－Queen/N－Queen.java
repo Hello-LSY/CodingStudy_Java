385652cb737be78ce0bc30ddc86c7fe7d09ac40a
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-09 13:44:56 +0900

    [level 2] Title: N-Queen, Time: 149.11 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.java
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -1,14 +1,10 @@
 class Solution {
-    private int answer;
-    private boolean[] cols;
-    private boolean[] d1; // 좌하향 대각선
-    private boolean[] d2; // 우하향 대각선
+    private static int answer;
+    private static int[] queens;
     
     public int solution(int n) {
         answer = 0;
-        cols = new boolean[n];
-        d1 = new boolean[2 * n - 1];
-        d2 = new boolean[2 * n - 1];
+        queens = new int[n];
         
         dfs(0, n);
         
@@ -22,19 +18,22 @@ class Solution {
         }
         
         for (int col = 0; col < n; col++) {
-            int id1 = row - col + n - 1;
-            int id2 = row + col;
-            if (cols[col] || d1[id1] || d2[id2]) {
-                continue;
+            // 퀸 배치 가능한지 로직필요
+            if (isValid(row, col)) {
+                queens[row] = col;
+                dfs(row + 1, n);
+            }
+        }
+    }
+    
+    private boolean isValid(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            // i번째 행의 퀸과 현재 배치하려는 퀸의 행,열차이 
+            if (queens[i] == col || Math.abs(queens[i] - col) == Math.abs(i - row)) {
+                return false;
             }
             
-            cols[col] = true;
-            d1[id1] = true;
-            d2[id2] = true;
-            dfs(row + 1, n);
-            cols[col] = false;
-            d1[id1] = false;
-            d2[id2] = false;
         }
+        return true;
     }
 }
