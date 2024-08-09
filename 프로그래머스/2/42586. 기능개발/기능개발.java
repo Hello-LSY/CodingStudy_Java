13dba11feb548be9bcfa6012990eee4ab5741c89
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-09 09:58:22 +0900

    [level 2] Title: 기능개발, Time: 0.55 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,37 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> result = new ArrayList<>();
-        Queue<Integer> que = new LinkedList<>();
         
+        Queue<Integer> que = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<progresses.length; i++){
-            // 걸리는 일수를 큐에 넣기
-            int day = (int)Math.ceil((100.0 - progresses[i])/speeds[i]);
+            // 소수점으로 남아도 하루 더 해야함
+            int day = (int)Math.ceil((100.0-progresses[i])/speeds[i]);
             que.offer(day);
         }
         
-        int progress = que.poll();
-        // 첫 작업은 이미 함
-        int cnt = 1; 
-        
         while(!que.isEmpty()){
-            if(progress >= que.peek()){
-                cnt++;
+            int work = que.poll();
+            int cnt = 1;
+            while(!que.isEmpty() && work>=que.peek()){
                 que.poll();
-            } else {
-                result.add(cnt);
-                cnt = 1;  
-                progress = que.poll();
+                cnt++;
             }
+            list.add(cnt);
         }
-        // 마지막 작업
-        result.add(cnt);
+        int[] answer = new int[list.size()];
         
-        int[] answer = new int[result.size()];
-        for(int i=0; i<result.size(); i++){
-            answer[i] = result.get(i);
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
         }
+        
         
         return answer;
     }
