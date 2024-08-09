a4d2cb52d3031536c4f79a3cda33198292657045
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-09 11:06:52 +0900

    [level 3] Title: 가장 먼 노드, Time: 61.43 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,53 +2,55 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        // 그래프를 인접 리스트 형태로 생성
+        int answer = 0;
+        int maxDis = 0;
         List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        // 간선의 수 = n + 1
+        // 노드 추가
+        for(int i=0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
         
-        for (int[] e : edge) {
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
+        // 간선 연결
+        for(int[] e : edge){
+            int x = e[0];
+            int y = e[1];
+            graph.get(x).add(y);
+            graph.get(y).add(x);
         }
         
-        // BFS를 이용해 최단 거리 계산
-        int[] dist = bfs(graph, n);
-        
-        // 가장 멀리 떨어진 노드의 개수를 계산
-        int maxDist = 0;
-        int cnt = 0;
-        
-        for (int d : dist) {
-            if (d > maxDist) {
-                maxDist = d;
-                cnt = 1;
-            } else if (d == maxDist) {
-                cnt++;
+        int[]dis = bfs(graph, n);
+        for(int d : dis){
+            if(d>maxDis){
+                maxDis=d;
+                answer=1;
+            }else if(d==maxDis){
+                answer++;
             }
         }
         
-        return cnt;
+        return answer;
     }
-
-    private int[] bfs(List<List<Integer>> graph, int n) {
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, -1);
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
-        dist[1] = 0;
+    
+    
+    private int[] bfs(List<List<Integer>> graph, int n){
+        int[] dis = new int[n+1];
+        boolean[] visited = new boolean[n+1];
         
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int next : graph.get(cur)) {
-                if (dist[next] == -1) {
-                    dist[next] = dist[cur] + 1;
-                    q.offer(next);
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(1);
+        visited[1]=true;
+        
+        while(!que.isEmpty()){
+            int cur = que.poll();
+            for(int next : graph.get(cur)){
+                if(!visited[next]){
+                    dis[next] = dis[cur] + 1;
+                    que.offer(next);
+                    visited[next]=true;
                 }
             }
         }
-        
-        return dist;
+        return dis;
     }
 }
