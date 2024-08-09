63f843386c65663ff9ad2ae0df2d04e1ee7bce29
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-09 10:22:20 +0900

    [level 2] Title: 괄호 회전하기, Time: 35.97 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,47 +1,34 @@
 import java.util.*;
-
 class Solution {
-    
     public int solution(String s) {
         int answer = 0;
         
         for(int i=0; i<s.length(); i++){
-            if(isBracket(rotate(s, i))){
-                answer++;
-            }
+            if(isBracket(rotate(s, i))) answer++; 
         }
-        
         
         return answer;
     }
     
-    //선형 완탐 로직
     private boolean isBracket(String s){
         Stack<Character> stk = new Stack<>();
-        
-        for(int i=0; i<s.length(); i++){
-            char cur = s.charAt(i);
-            if(cur=='{' || cur=='[' || cur=='('){
-                stk.push(cur);
-            } else{
-                if(stk.isEmpty()) return false;
-                char top = stk.pop();
-                if((cur=='}' && top != '{') || 
-                   (cur==']' && top!='[') || 
-                   (cur==')' && top!='(')){
-                    return false;
-                }
+        char[] cArr = s.toCharArray();
+        for(char c : cArr){
+            if(c=='{' || c=='[' || c=='('){
+                stk.push(c);
+            }else{
+                if(stk.isEmpty())return false;
+                if((c=='}' && stk.peek()=='{') || 
+                   (c==']' && stk.peek()=='[') ||
+                   (c==')' && stk.peek()=='(')) stk.pop();
+                else return false;
             }
         }
-        
         return stk.isEmpty();
     }
     
     
-    //회전하는 로직
-    private String rotate(String s, int step){
-        return s.substring(step) + s.substring(0, step);
+    private String rotate(String s, int idx){
+        return s.substring(idx) + s.substring(0, idx);
     }
-    
-    
 }
