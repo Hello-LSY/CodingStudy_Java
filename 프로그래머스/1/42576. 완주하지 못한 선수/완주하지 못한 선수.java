a820d12a7fe4a984c6aff2a7042a74c382e946af
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-09 09:38:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.48 ms, Memory: 95.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,6 +1,22 @@
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String part : participant){
+            map.put(part, map.getOrDefault(part, 0)+1);
+        }
+        
+        for(String comp : completion){
+            map.replace(comp, map.get(comp)-1);
+        }
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue()!=0) return entry.getKey();
+        }
+        
         return answer;
     }
 }
