6edf3e3b74fa81ef1ceaf913d9dd2e00ccb4c9af
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-09 10:48:45 +0900

    [level 2] Title: 후보키, Time: 6.59 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -3,86 +3,43 @@ import java.util.*;
 class Solution {
 
     public int solution(String[][] relation) {
-        int n = relation[0].length; 
-        List<List<Integer>> candidates = new ArrayList<>();
-        
-        // 컬럼 조합 생성
-        CombinationGenerator combGen = new CombinationGenerator();
-        for (int i = 1; i <= n; i++) {
-            candidates.addAll(combGen.combine(n, i));
-        }
-        
-        List<List<Integer>> uniqueKeys = getUniqueKeys(candidates, relation);
-        
-        List<List<Integer>> resultKeys = getMinimalKeys(uniqueKeys);
-        
-        return resultKeys.size();
-    }
-    
-    //유일성 검사
-    private List<List<Integer>> getUniqueKeys(List<List<Integer>> candidates, String[][] relation) {
-        List<List<Integer>> uniqueKeys = new ArrayList<>();
-        
-        for (List<Integer> candidate : candidates) {
-            //중복없애기
-            Set<String> set = new HashSet<>();
-            for (String[] row : relation) {
-                StringBuilder sb = new StringBuilder();
-                for (int col : candidate) {
-                    sb.append(row[col - 1]).append(",");
-                }
-                set.add(sb.toString());
-            }
-            if (set.size() == relation.length) {
-                uniqueKeys.add(candidate);
-            }
-        }
-        
-        return uniqueKeys;
-    }
-    
-    //최소성 검사
-    private List<List<Integer>> getMinimalKeys(List<List<Integer>> uniqueKeys) {
-        List<List<Integer>> resultKeys = new ArrayList<>();
-        
-        for (List<Integer> key : uniqueKeys) {
-            boolean isMinimal = true;
-            for (List<Integer> resultKey : resultKeys) {
-                if (key.containsAll(resultKey)) {
-                    isMinimal = false;
-                    break;
-                }
-            }
-            if (isMinimal) {
-                resultKeys.add(key);
-            }
-        }
-        
-        return resultKeys;
-    }
-    
-    // 조합 생성
-    class CombinationGenerator {
-        private List<List<Integer>> comb;
-        
-        public List<List<Integer>> combine(int n, int k) {
-            comb = new ArrayList<>();
-            List<Integer> curComb = new ArrayList<>();
-            dfs(n, k, 1, curComb);
-            return comb;
-        }
-        
-        private void dfs(int n, int k, int start, List<Integer> curComb) {
-            if (curComb.size() == k) {
-                comb.add(new ArrayList<>(curComb));
-                return;
-            }
+        int n = relation[0].length;
+        List<Integer> candidateKeys = new ArrayList<>();
 
-            for (int i = start; i <= n; i++) {
-                curComb.add(i);
-                dfs(n, k, i + 1, curComb);
-                curComb.remove(curComb.size() - 1);
+        for (int i = 1; i < (1 << n); i++) {
+            if (isUnique(i, relation) && isMinimal(i, candidateKeys)) {
+                candidateKeys.add(i);
             }
         }
+
+        return candidateKeys.size();
+    }
+
+    // 유일성 검사
+    private boolean isUnique(int subset, String[][] relation) {
+        Set<String> seen = new HashSet<>();
+        
+        for (String[] row : relation) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < relation[0].length; j++) {
+                if ((subset & (1 << j)) != 0) {
+                    sb.append(row[j]).append(",");
+                }
+            }
+            if (!seen.add(sb.toString())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 최소성 검사
+    private boolean isMinimal(int subset, List<Integer> candidateKeys) {
+        for (int key : candidateKeys) {
+            if ((subset & key) == key) {
+                return false;
+            }
+        }
+        return true;
     }
 }
