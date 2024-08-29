3a2b7e2900d78b55a7615e4a235333843844812a
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2024-08-29 19:45:58 +0900

    [level 2] Title: 소수 찾기, Time: 37.99 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,37 +1,32 @@
 import java.util.*;
-
 class Solution {
     public int solution(String numbers) {
-        //HashSet : 중복저장안됨
-        Set<Integer> primeSet = new HashSet<>();
+        int answer = 0;
         boolean[] visited = new boolean[numbers.length()];
-        char[] chars = numbers.toCharArray();
-        int answer=0;
-        dfs(chars, "", visited, primeSet);
-        for(int num : primeSet){
-            if(isPrime(num)) answer++;
+        Set<Integer> hashSet = new HashSet<>();
+        dfs(numbers.toCharArray(), "", visited, hashSet);
+        
+        for(int i : hashSet){
+            if(isPrime(i)) answer++;
         }
-
+        
         return answer;
     }
-
-    // 순열 생성
-    private void dfs(char[] chars, String cur, boolean[] visited, Set<Integer> primeSet){
-        if(!cur.equals("")){
-            primeSet.add(Integer.parseInt(cur));
+    
+    private void dfs(char[] charArr, String result, boolean[] visited, Set<Integer> hashSet){
+        if(!result.equals("")){
+            hashSet.add(Integer.parseInt(result));
         }
         
-        for(int i=0; i<chars.length; i++){
+        for(int i=0; i<charArr.length; i++){
             if(!visited[i]){
-                visited[i] = true;
-                dfs(chars, cur+chars[i], visited, primeSet);
-                visited[i] = false;
+                visited[i]=true;
+                dfs(charArr, result+charArr[i], visited, hashSet);
+                visited[i]=false;
             }
         }
-        
     }
-
-    // 소수 여부 확인
+    
     private boolean isPrime(int n){
         if(n<2) return false;
         for(int i=2; i*i<=n; i++){
@@ -39,4 +34,5 @@ class Solution {
         }
         return true;
     }
+    
 }
