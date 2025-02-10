845e01408c9accceccb51c5eaed20cb8afe1379d
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2025-02-11 02:41:44 +0900

    Time: 1 ms (92.89%), Space: 44.6 MB (63.97%) - LeetHub

--- LeetCode/0046-permutations/0046-permutations.java
+++ LeetCode/0046-permutations/0046-permutations.java
@@ -1,36 +1,24 @@
-import java.util.*;
-
 class Solution {
-    private boolean[] visited;
-    private List<List<Integer>> perm;
-
     public List<List<Integer>> permute(int[] nums) {
-        visited = new boolean[nums.length];
-        perm = new ArrayList<>();
-        List<Integer> curPerm = new ArrayList<>();
-        
-        dfs(nums, curPerm);
-        
-        return perm;
+        List<List<Integer>>result = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
+        dfs(nums, new ArrayList<>(), result, visited);
+        return result;
     }
-
-    private void dfs(int[] nums, List<Integer> curPerm) {
-        //순열생성됐으면 넣음
-        if (curPerm.size() == nums.length) {
-            perm.add(new ArrayList<>(curPerm));
+    
+    private void dfs(int[] nums, List<Integer> num, List<List<Integer>>result, boolean[]visited){
+        if(num.size()==nums.length){
+            result.add(new ArrayList<>(num));
             return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                curPerm.add(nums[i]);
-
-                dfs(nums, curPerm);
-
-                curPerm.remove(curPerm.size() - 1);
-                visited[i] = false;
-
+        for(int i=0; i<nums.length; i++){
+            if(!visited[i]){
+                visited[i]=true;
+                num.add(nums[i]);
+                dfs(nums, num, result, visited);
+                num.remove(num.size()-1);
+                visited[i]=false;
             }
         }
     }
