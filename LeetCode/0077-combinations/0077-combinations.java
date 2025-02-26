d1b9f4b854e39b2c051b9750195977eecc999fc6
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2025-02-26 11:01:52 +0900

    Time: 18 ms (79.59%), Space: 94.6 MB (73.93%) - LeetHub

--- LeetCode/0077-combinations/0077-combinations.java
+++ LeetCode/0077-combinations/0077-combinations.java
@@ -1,36 +1,23 @@
-import java.util.*;
-
 class Solution {
-    private List<List<Integer>> comb;
-    // private boolean[] visited;
-
     public List<List<Integer>> combine(int n, int k) {
-        comb = new ArrayList<>();
-        // visited = new boolean[n + 1];
-        List<Integer> curComb = new ArrayList<>();
-        
-        dfs(n, k, 1, curComb);
-        
-        return comb;
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(n, k, 1, new ArrayList<>(), result);
+
+        return result;
     }
 
-    private void dfs(int n, int k, int start, List<Integer> curComb) {
-        //완성되면 넣기
-        if (curComb.size() == k) {
-            comb.add(new ArrayList<>(curComb));
+    private void dfs(int n, int k, int start, List<Integer>nums, List<List<Integer>>result){
+        if(nums.size()==k){
+            result.add(new ArrayList<>(nums));
             return;
         }
 
-        for (int i = start; i <= n; i++) {
-            
-                // visited[i] = true;
-                curComb.add(i);
-                
-                dfs(n, k, i + 1, curComb);
-                
-                curComb.remove(curComb.size() - 1);
-                // visited[i] = false;
-            
+        for(int i=start; i<=n; i++){
+            nums.add(i);
+            dfs(n,k,i+1,nums,result);
+            nums.remove(nums.size()-1);
         }
     }
+
+
 }
