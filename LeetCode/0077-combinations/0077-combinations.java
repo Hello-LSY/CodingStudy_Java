17ea1ca61bac0cf80bda1486b90761f0dc6aa9a6
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2025-02-27 11:03:32 +0900

    Time: 27 ms (18.03%), Space: 94.7 MB (41.9%) - LeetHub

--- LeetCode/0077-combinations/0077-combinations.java
+++ LeetCode/0077-combinations/0077-combinations.java
@@ -1,23 +1,21 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
+        List<Integer> list = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
-        dfs(n, k, 1, new ArrayList<>(), result);
+        dfs(n,k,1, list, result);
 
         return result;
     }
 
-    private void dfs(int n, int k, int start, List<Integer>nums, List<List<Integer>>result){
-        if(nums.size()==k){
-            result.add(new ArrayList<>(nums));
-            return;
+    private void dfs(int n, int k, int start, List<Integer> list, List<List<Integer>>result){
+        if(list.size()==k){
+            result.add(new ArrayList<>(list));
         }
 
-        for(int i=start; i<=n; i++){
-            nums.add(i);
-            dfs(n,k,i+1,nums,result);
-            nums.remove(nums.size()-1);
+        for(int i = start; i<n+1; i++){
+            list.add(i);
+            dfs(n, k, i+1, list, result);
+            list.remove(list.size()-1);
         }
     }
-
-
 }
