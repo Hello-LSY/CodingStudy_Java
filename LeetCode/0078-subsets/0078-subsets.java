006ce44e4252bb4fe838c7b4436c0c85e90dfe4b
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2025-03-03 14:48:22 +0900

    Time: 1 ms (54.1%), Space: 42.9 MB (46.07%) - LeetHub

--- LeetCode/0078-subsets/0078-subsets.java
+++ LeetCode/0078-subsets/0078-subsets.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>result = new ArrayList<>();
-        dfs(nums, 0, new ArrayList<>(), result);
+
+        dfs(nums, 0, result, new ArrayList<>());
 
         return result;
     }
 
-    private void dfs(int[]nums, int start, List<Integer>list, List<List<Integer>>result){
+    private void dfs(int[] nums, int start, List<List<Integer>>result, List<Integer> list){
         result.add(new ArrayList<>(list));
 
         for(int i=start; i<nums.length; i++){
             list.add(nums[i]);
-            dfs(nums, i+1, list, result);
+            dfs(nums, i+1, result, list);
             list.remove(list.size()-1);
         }
 
-    } 
+    }
 }
