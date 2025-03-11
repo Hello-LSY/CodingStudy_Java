cf40c6a1f15b07f2259aa15f26adbe165a4a0295
Hello-LSY <81401604+Hello-LSY@users.noreply.github.com>
2025-03-11 23:26:44 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = Math.min(n, Integer.parseInt(st.nextToken())/2 + Integer.parseInt(st.nextToken()));
+        System.out.print(result);
+    }
+}
